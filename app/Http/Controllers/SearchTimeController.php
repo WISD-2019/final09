f2029b9b77e5fe470af5520cb51c0317167ded2d
林威震 <weizhen19990727@gmail.com>
2020-01-01 04:19:51 +0800

    新增訂票和時刻查詢的頁面

---
+++ app/Http/Controllers/SearchTimeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SearchTimeController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('searchtime');
+    }
+}
